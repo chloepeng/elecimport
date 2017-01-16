fdf0a39b22a761e3d6fa3bdcea6ee7d09faf7390
chloepeng <pyiyj@aliyun.com>
2017-01-16 09:53:29 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function reset(){
+  document.getElementById('contactName').value = '';
+  document.getElementById('contactEmail').value = '';
+  document.getElementById('contactComment').value = '';
+}
